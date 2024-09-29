c2ee5981055ab5a142e0524d69a729aa6e49b1f8
prjctimg <prjctimg@outlook.com>
2024-09-29 16:38:57 +0200

    new post

--- next-env.d.ts
+++ next-env.d.ts
@@ -3,3 +3,19 @@
 
 // NOTE: This file should not be edited
 // see https://nextjs.org/docs/basic-features/typescript for more information.
+
+
+class SomeObject {
+
+    constructor(opts){
+      this.opts = opts
+    }
+    
+    getKeys(){
+    return Object.keys(this.opts)
+    }
+  };
+  
+  const obj = new SomeObject(),x = obj instanceof SomeObject
+
+  console.log(x)
